b6c4c58bb6a33318280e4d2c3d46b4efa1bfbf25
wafulo <30869718+wafulo@users.noreply.github.com>
2017-10-19 15:11:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ io.on('connection', function(socket) {
     socket.on('error', (error) => {
       console.log("chat message", "error")
     });
-    socket.on('disconnect', (reason) => {sfgdsfgdsgf
+    socket.on('disconnect', (fasdfsdf) => {
       console.log("chat message", "disconnect")
     });
 
